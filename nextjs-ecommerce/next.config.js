01a29c76de181d7426b0b4a714ba48808c4d27d9
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-22 17:46:47 +0530

    few changes in next.config

--- nextjs-ecommerce/next.config.js
+++ nextjs-ecommerce/next.config.js
@@ -1,12 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 module.exports = {
+  nextConfig,
   env: {
     'BASE_URL': 'http://localhost:3000',
     'MONGODB_URL': 'mongodb+srv://root:root123@cluster0.8qqva.mongodb.net/nextjs-ecommerce?retryWrites=true&w=majority',
