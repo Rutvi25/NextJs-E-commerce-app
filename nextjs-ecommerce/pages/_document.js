160251ea397f2c33bed7eee4612d44e4c1e1c991
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-26 11:43:45 +0530

    added css file for font awesome icons

--- nextjs-ecommerce/pages/_document.js
+++ nextjs-ecommerce/pages/_document.js
@@ -6,12 +6,23 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta name='description' content='E-commerce website with NextJs' />
-          <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css' />   
+          <link
+            rel='stylesheet'
+            href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css'
+          />
           <script src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js'></script>
           <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js'></script>
-          <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css'
+            integrity='sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=='
+            crossorigin='anonymous'
+            referrerpolicy='no-referrer'
+          />
           <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-          <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}&currency=USD`}></script>
+          <script
+            src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}&currency=USD`}
+          ></script>
         </Head>
         <body>
           <Main />
