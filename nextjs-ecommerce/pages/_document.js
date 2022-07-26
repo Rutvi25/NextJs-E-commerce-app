5c0aba4790ab2a1d912e2daa7d6883d262f113c0
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-26 11:35:45 +0530

    added css file for font awesome icons

--- nextjs-ecommerce/pages/_document.js
+++ nextjs-ecommerce/pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
           <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css' />   
           <script src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js'></script>
           <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js'></script>
+          <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
           <script src='https://kit.fontawesome.com/a076d05399.js'></script>
           <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}&currency=USD`}></script>
         </Head>
