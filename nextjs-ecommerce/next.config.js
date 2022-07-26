08cd4b08eb9345fd141212453227cbe05ade70b3
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-26 10:56:42 +0530

    base url changed

--- nextjs-ecommerce/next.config.js
+++ nextjs-ecommerce/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   env: {
-    'BASE_URL': 'https://next-js-e-commerce-app-six.vercel.app',
+    'BASE_URL': 'https://next-js-e-commerce-app-six.vercel.app/',
     'MONGODB_URL': 'mongodb+srv://root:root123@cluster0.8qqva.mongodb.net/nextjs-ecommerce?retryWrites=true&w=majority',
     'ACCESS_TOKEN_SECRET': 'U!UeRea%0Yn=+KHTW>Z*U8H0Jj%VMgstZY1jHnANUnt1d.P?Q',
     'REFRESH_TOKEN_SECRET': 'Bu+i9N+jsr*>Jdf0D466:4pX?gNj1M4Xv.~Qb_+m9cjr@#FutQq3LcmR~WNpGm=J19^pyV?uJ.A:@K',
